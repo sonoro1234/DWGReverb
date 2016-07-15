82fb86810cf132a70b8ad6953d17d7f4a0bc5736
Victor Bombi <sonoro@telefonica.net>
2016-07-15 18:42:56 +0200

    update build install

--- build/init_cmake.bat
+++ build/init_cmake.bat
@@ -1,3 +1,3 @@
 
-cmake  -G"MinGW Makefiles" -DSC_PATH=""C:/SupercolliderRepos/master4/supercollider"" -DCMAKE_BUILD_TYPE="Release"   ..
+cmake  -G"MinGW Makefiles" -DSC_PATH=""C:/SupercolliderRepos/scfork/supercollider"" -DCMAKE_INSTALL_PREFIX="C:\Users\victor\AppData\Local\SuperCollider\Extensions" -DSUPERNOVA=YES -DCMAKE_BUILD_TYPE="Release"   ..
  
